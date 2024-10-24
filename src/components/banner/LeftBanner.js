f2e61876faef16770747d7f84511dede82ad2466
SANTHANAM <santhanam.ibm@gmail.com>
2024-10-24 10:57:28 +0530

    Name & Heading Chnage

--- src/components/banner/LeftBanner.js
+++ src/components/banner/LeftBanner.js
@@ -14,9 +14,9 @@ const LeftBanner = () => {
     <div className="w-full lgl:w-1/2 flex flex-col gap-20">
       <div className="flex flex-col gap-5">
         <h4 className=" text-lg font-normal">WELCOME TO MY WORLD</h4>
-        <h1 className="text-6xl font-bold text-white">
-          Hi, I'm <span className="text-designColor capitalize">Santhanam</span>
-        </h1>
+        <h3 className="text-6xl font-bold text-white">
+          Hi, I'm <span className="text-designColor capitalize">Sandel</span>
+        </h3>
         <h2 className="text-4xl font-bold text-white">
           a <span>{text}</span>
           <Cursor
