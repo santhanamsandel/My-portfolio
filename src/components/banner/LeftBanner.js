66da28a951f9f9361e391d59e405121a59970fed
santhanamsandel <107601976+santhanamsandel@users.noreply.github.com>
2024-10-24 12:01:56 +0530

    LeftBanner.js
    Name change

--- src/components/banner/LeftBanner.js
+++ src/components/banner/LeftBanner.js
@@ -15,7 +15,7 @@ const LeftBanner = () => {
       <div className="flex flex-col gap-5">
         <h4 className=" text-lg font-normal">WELCOME TO MY WORLD</h4>
         <h3 className="text-6xl font-bold text-white">
-          Hi, I'm <span className="text-designColor capitalize">Sandel</span>
+          Hi, I'm <span className="text-designColor capitalize">SANTHANAM</span>
         </h3>
         <h2 className="text-4xl font-bold text-white">
           a <span>{text}</span>
